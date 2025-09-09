496bdbe2981546c873336b61ab3ab1a19deb3477
KhaliD0225012 <aleph3371@gmail.com>
2025-09-09 11:30:52 +0800

    Create Day2Coding.java

---
+++ Day2Coding.java
@@ -0,0 +1,19 @@
+package day2coding;
+
+public class Day2Coding {
+
+    public static void main(String[] args) {
+        System.out.print("Day\s");
+        System.out.print("2\s");
+        System.out.print("Coding\n\n");
+        
+        System.out.println("Penggunaan:");
+        System.out.println("1. print");
+        System.out.println("2. println");
+        System.out.println("3. printf");
+        
+        System.out.printf("\nNama: %s\nUmur: %d\n", "Khalid", 19);
+        
+    }
+    
+}
