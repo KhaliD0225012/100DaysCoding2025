1ed3f8d15926a954ae9f890dd6da3340e5aab0d5
KhaliD0225012 <aleph3371@gmail.com>
2025-09-10 10:51:20 +0800

    Create Day3Coding.java

---
+++ Day3Coding.java
@@ -0,0 +1,26 @@
+//Bagian package atau nama dari file
+package day3coding;
+
+
+public class Day3Coding {
+
+    //Memiliki shortcut "psvm + tab"
+    public static void main(String[] args) {
+        
+        //Tipe data String digunakan untuk menyimpan data seperti nama dan nomor yang tidak dioperasikan
+        String nama = "Khalid";
+        String NIM = "D0225012";
+                
+        /*
+        print adalah perintah mencetak tanpa menambah baris baru
+        println adalah perintah mencetak dengan menambahkan baris baru
+        printf adalah perintah mencetak dengan ketentuan khusus
+        */
+        System.out.print(nama);
+        System.out.println(NIM);
+        System.out.printf("Nama: %sNIM: %s", nama, NIM);
+        
+        
+    }
+    
+}
