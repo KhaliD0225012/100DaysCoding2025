46cd6ce4be4154c5450991946976ae88aeb78fde
KhaliD0225012 <aleph3371@gmail.com>
2025-09-11 12:49:15 +0800

    Update Day4Coding.java

--- Day4Coding.java
+++ Day4Coding.java
@@ -4,16 +4,16 @@ public class Day4Coding {
 	
 	public static void main(String[] args) {
 		
-		// newline (/n), menambahkan baris baru setelah mencetak
-		System.out.printf("\nNama: %s\nNIM: %s\n", "Khalid", "DO225012");
+		// newline (\n), menambahkan baris baru setelah mencetak
+		System.out.printf("Nama: %s\nNIM: %s\n", "Khalid", "DO225012");
 		
-		//space (/s), menambahkan spasi setelah mencetak
+		//space (\s), menambahkan spasi setelah mencetak
 		System.out.println("Fakultas:\s", "Teknik");
 		
-		//tab (/t), menambah tab atau spasi lebar setelah mencetak
+		//tab (\t), menambah tab atau spasi lebar setelah mencetak
 		System.out.println("Prodi:\t", "Informatika");
 		
-		//backspace (/b), menghapus satu variable/karakter sebelumnya
+		//backspace (\b), menghapus satu variable/karakter sebelumnya
 		System.out.println("Kelas: BE\b");
 	}
 }
