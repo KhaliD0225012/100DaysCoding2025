67929af0e95e96557b3765b38f80c8566571b5be
KhaliD0225012 <aleph3371@gmail.com>
2025-09-12 22:03:32 +0800

    Create Day5Coding.java

---
+++ Day5Coding.java
@@ -0,0 +1,19 @@
+package day5coding;
+
+public class Day5Coding {
+
+    public static void main(String[] args) {
+        
+        byte A = 127;
+        short B = 32000;
+        int C = 170000000;
+        long D = 900000000;
+        
+        System.out.printf("Byte: %d\n", A);
+        System.out.printf("Short; %d\n", B);
+        System.out.printf("int: %d\n", C);
+        System.out.printf("long; %d\n", D);
+        
+    }
+    
+}
