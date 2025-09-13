5ce8e245f8d230c8a85b6c500155f6cc6f852c4f
KhaliD0225012 <aleph3371@gmail.com>
2025-09-13 20:25:35 +0800

    Create Day6Coding.java

---
+++ Day6Coding.java
@@ -0,0 +1,15 @@
+package day6coding;
+
+public class Day6Coding {
+
+    public static void main(String[] args) {
+        
+        float A = 3.141593f;
+        double B = 1.618034;
+        
+        System.out.printf("Pi: %f\n", A);
+        System.out.printf("Golden Ratio: %f\n", B);
+        
+    }
+    
+}
