603a7064bacf7ab0fbb8c469ed47dbdd7156336f
KhaliD0225012 <aleph3371@gmail.com>
2025-09-14 11:26:40 +0800

    Create Day&Coding.java

---
+++ Day&Coding.java
@@ -0,0 +1,21 @@
+package day7coding;
+
+public class Day7Coding {
+
+   public static void main(String[] args) {
+        
+       char variabel1 = 'A';
+       char variabel2 = 'C';
+       boolean A = false;
+       boolean C = true;
+       
+       if (A){
+           System.out.printf("Huruf Setelah B adalah: %5c\n", variabel1);
+       } else {
+           System.out.printf("Huruf Setelah B adalah: %5c\n", variabel2);
+       }
+       
+       
+    }
+    
+}
