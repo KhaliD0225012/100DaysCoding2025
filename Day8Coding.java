5248b640fbfe72bae0d272d41187f873b3c1acb4
KhaliD0225012 <aleph3371@gmail.com>
2025-09-15 11:30:54 +0800

    Create Day8Coding.java

---
+++ Day8Coding.java
@@ -0,0 +1,21 @@
+package day8coding;
+
+public class Day8Coding {
+
+   public static void main(String[] args) {
+        
+       String nama = "Abdul Khalid",
+       fakultas = "Teknik",
+       prodi = "Informatika",
+       kelas = "B",
+       nim = "D0225012";
+       
+       System.out.printf("Nama \t\t: %s\n", nama);
+       System.out.printf("Fakultas\t: %s\n", fakultas);
+       System.out.printf("Prodi \t\t: %s\n", prodi);
+       System.out.printf("Kelas \t\t: %s\n", kelas);
+       System.out.printf("NIM \t\t: %s\n", nim);
+       
+    }
+    
+}
