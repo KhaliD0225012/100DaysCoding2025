f4a382f1bf751bcc68689ffb34cc243698561382
KhaliD0225012 <aleph3371@gmail.com>
2025-09-16 11:33:31 +0800

    Create Day9Coding.java

---
+++ Day9Coding.java
@@ -0,0 +1,22 @@
+package day9coding;
+
+public class Day9Coding {
+
+    public static void main(String[] args) {
+        
+        String nama;
+        int umur;
+        
+        nama = "Khalid";
+        umur = 18;
+        System.out.println("Deklarasi Awal");
+        System.out.printf("Nama\t: %s\nUmur\t: %d\n\n", nama, umur);
+        
+        nama = "Abdul Khalid";
+        umur = 19;
+        System.out.println("Update Variabel");
+        System.out.printf("Nama\t: %s\nUmur\t: %d", nama, umur);
+        
+    }
+    
+}
