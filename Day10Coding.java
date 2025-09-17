da9f7767155f5cb536ea184a441c0e54a2d5f7e7
KhaliD0225012 <aleph3371@gmail.com>
2025-09-17 09:14:15 +0800

    Create Day10Coding.java

---
+++ Day10Coding.java
@@ -0,0 +1,26 @@
+package day10coding;
+
+public class Day10Coding {
+
+    public static void main(String[] args) {
+        
+        final int umur = 19; 
+        final double tinggi = 168.4;
+        final char darah = 'O';
+        final boolean kuliah = true;
+        
+        System.out.printf("Nama\t\t: %s\nUmur\t\t: %d\n", "Abdul Khalid", umur);
+        System.out.printf("Tinggi Badan\t: %s\n", tinggi);
+        System.out.printf("Golongan Darah\t: %c\nLanjut Kuliah\t: %b\n", darah, kuliah);
+        
+        /*
+          umur = 20;
+          tinggi = 170;
+          darah = O+;
+          kuliah = false;
+        Program akan error jika nilai variabel diupdate
+        */
+        
+    }
+    
+}
