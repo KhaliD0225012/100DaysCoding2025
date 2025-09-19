0407e52b6c58d71fa0a8375e4c88de3f2c3cfa88
KhaliD0225012 <aleph3371@gmail.com>
2025-09-19 11:29:03 +0800

    Create Day12Coding.java

---
+++ Day12Coding.java
@@ -0,0 +1,41 @@
+package day12coding;
+
+import java.util.Scanner;
+
+public class Day12Coding {
+
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+        System.out.println("Biodata");
+        
+        System.out.print("Nama Lengkap\t: ");
+        String nama = input.nextLine();
+        
+        System.out.print("Jenis Kelamin\t: ");
+        String gender = input.nextLine();
+        
+        System.out.print("Tempat Lahir\t: ");
+        String tempat = input.nextLine();
+        
+        System.out.print("Tanggal Lahir\t: ");
+        String tanggal = input.nextLine();
+       
+        System.out.print("Agama\t\t: ");
+        String agama = input.nextLine();
+        
+        System.out.print("Alamat\t\t: ");
+        String alamat = input.nextLine();
+        
+        System.out.print("Golongan Darah\t: ");
+        String darah = input.nextLine();
+        
+        System.out.print("Status Hubungan\t: ");
+        String status = input.nextLine();
+        
+        System.out.print("Pekerjaan\t: ");
+        String kerja = input.nextLine();
+        
+    }
+    
+}
