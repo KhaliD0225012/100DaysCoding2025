da35fc8ad75c88e4bbf62c93acc7ef098c40f97c
KhaliD0225012 <aleph3371@gmail.com>
2025-09-25 21:16:33 +0800

    Create Day18Coding.java

---
+++ Day18Coding.java
@@ -0,0 +1,19 @@
+package day18coding;
+
+public class Day18Coding {
+
+    public static void main(String[] args) {
+        
+        byte angka0 = 120;
+        short angka1 = angka0;
+        int angka2 = angka1;
+        double angka3 = angka2;
+        
+        System.out.println("Tipe Data Byte\t\t: " + angka0);
+        System.out.println("Tipe Data Short\t\t: " + angka1);
+        System.out.println("Tipe Data Integer\t: " + angka2);
+        System.out.println("Tipe Data double\t: " + angka3);        
+        
+    }
+    
+}
