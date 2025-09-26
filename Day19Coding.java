47edfd36b57c60f0bdc61fe8f6a9885cd2f43ffd
KhaliD0225012 <aleph3371@gmail.com>
2025-09-26 23:13:34 +0800

    Update Day19Coding.java

--- Day19Coding.java
+++ Day19Coding.java
@@ -7,16 +7,16 @@ public class Day19Coding {
     public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
-        System.out.print("Masukkan Nilai Variabel Pertama");
-        double var1 = in.nextDouble();
+        System.out.print("Masukkan Nilai Variabel Pertama : ");
+        long var1 = in.nextLong();
         int var2 = (int)var1;
         short var3 = (short)var2;
         byte var4 = (byte)var3;
         
-        System.out.println("Nilai Variabel Pertama\t: ");
-        System.out.println("Nilai Variabel Kedua\t: ");
-        System.out.println("Nilai Variabel Ketiga\t: ");
-        System.out.println("Nilai Variabel Keempat\t: ");
+        System.out.println("Nilai Variabel Pertama\t: " + var1);
+        System.out.println("Nilai Variabel Kedua\t: " + var2);
+        System.out.println("Nilai Variabel Ketiga\t: " + var3);
+        System.out.println("Nilai Variabel Keempat\t: " + var4);
         
     }
     
