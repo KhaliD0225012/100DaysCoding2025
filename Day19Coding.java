f91f2b03392fd2da20536cc6534f5b512286a4ab
KhaliD0225012 <aleph3371@gmail.com>
2025-09-26 20:03:59 +0800

    Create Day19Coding.java

---
+++ Day19Coding.java
@@ -0,0 +1,23 @@
+package day19coding;
+
+import java.util.Scanner;
+
+public class Day19Coding {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan Nilai Variabel Pertama");
+        double var1 = in.nextDouble();
+        int var2 = (int)var1;
+        short var3 = (short)var2;
+        byte var4 = (byte)var3;
+        
+        System.out.println("Nilai Variabel Pertama\t: ");
+        System.out.println("Nilai Variabel Kedua\t: ");
+        System.out.println("Nilai Variabel Ketiga\t: ");
+        System.out.println("Nilai Variabel Keempat\t: ");
+        
+    }
+    
+}
