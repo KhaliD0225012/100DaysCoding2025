0102e67a880ebaf48173cef46067311ee92e8924
KhaliD0225012 <aleph3371@gmail.com>
2025-10-10 21:03:36 +0800

    Create Day33Coding.java

---
+++ Day33Coding.java
@@ -0,0 +1,14 @@
+package day33coding;
+
+public class Day33Coding {
+
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = !a;
+        System.out.println("Nilai boolean a  = " + a);
+        System.out.println("Nilai boolean !a = " + !a);
+        System.out.println("Nilai boolean b  = " + b);
+    }
+    
+}
