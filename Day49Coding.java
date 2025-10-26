7937897818e1c6dd3558894bfe03d05b0cf808fa
KhaliD0225012 <aleph3371@gmail.com>
2025-10-26 16:43:13 +0800

    Create Day49Coding.java

---
+++ Day49Coding.java
@@ -0,0 +1,12 @@
+package day49coding;
+
+public class Day49Coding {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Apakah 1 lebih besar dari 2?");
+        String pernyataan = (1>2)? "Benar" : "Salah";
+        System.out.println(pernyataan);
+    }
+    
+}
