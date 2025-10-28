a479632614edddc8d27db263562feb878f79eeee
KhaliD0225012 <aleph3371@gmail.com>
2025-10-28 08:49:20 +0800

    Create Day51Coding.java

---
+++ Day51Coding.java
@@ -0,0 +1,18 @@
+package day51coding;
+
+public class Day51Coding {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i < 10; i++) {
+            System.out.print(i);
+            if (i%2==0){
+                System.out.println(" (GENAP)");
+            } else {
+                System.out.println(" (GANJIL)");
+            }
+        }
+        
+    }
+    
+}
