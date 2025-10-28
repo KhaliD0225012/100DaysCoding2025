e7b11db86b9011745469e9c7beef277e2163bc5e
KhaliD0225012 <aleph3371@gmail.com>
2025-10-28 20:37:43 +0800

    Create Latihan4.java

---
+++ Latihan4.java
@@ -0,0 +1,14 @@
+package latihan;
+
+public class Latihan4 {
+    
+    public static void main(String[] args) {
+        
+        String nama = "Marsya";
+        for (int i = 0; i < nama.length(); i++) {
+            System.out.println(nama.substring(i));
+        }
+        
+    }
+    
+}
