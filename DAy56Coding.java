7e6d78152a5ea83553824b0a5666bcc80da57440
KhaliD0225012 <aleph3371@gmail.com>
2025-11-02 20:39:20 +0800

    Create DAy56Coding.java

---
+++ DAy56Coding.java
@@ -0,0 +1,14 @@
+package day56coding;
+
+public class Day56Coding {
+
+    public static void main(String[] args) {
+        
+        int a = 1;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a <= 10);
+    }
+    
+}
