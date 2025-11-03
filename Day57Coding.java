30a0994b553e6367442d3d3e2eb6572a2c7c1a96
KhaliD0225012 <aleph3371@gmail.com>
2025-11-03 17:42:41 +0800

    Create Day57Coding.java

---
+++ Day57Coding.java
@@ -0,0 +1,18 @@
+package day57coding;
+
+public class Day57Coding {
+
+    public static void main(String[] args) {
+        
+        int i = 0;
+        while (i < 10){
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+            i++;
+        }
+        
+    }
+    
+}
