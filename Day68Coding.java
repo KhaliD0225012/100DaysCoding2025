63ca11cdb1d0a6dff057aee6d78242fd530a038d
KhaliD0225012 <aleph3371@gmail.com>
2025-11-14 22:59:55 +0800

    Create Day68Coding.java

---
+++ Day68Coding.java
@@ -0,0 +1,27 @@
+package day68coding;
+
+public class Day68Coding {
+    
+    public static void main (String[]args){
+        
+        int a = 10;
+        char c = 'o';
+        for (int i = 0; i <= a; i++){
+            for (int j = 0; j <= a*2; j++){
+                if (i == 0 || i == a || j == 0 || j == a*2){
+                    if (j%2==0){
+                        System.out.print(c);
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    System.out.print(" ");
+                }
+              
+            }
+          
+        }
+      
+    }
+    
+}
