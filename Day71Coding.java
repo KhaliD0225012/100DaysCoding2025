0ab0638cd44aca76e822135169c44476013ae45d
KhaliD0225012 <aleph3371@gmail.com>
2025-11-17 20:28:37 +0800

    Create Day71Coding.java

---
+++ Day71Coding.java
@@ -0,0 +1,16 @@
+package day71coding;
+
+public class Day71Coding {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("o ");
+            }
+            System.out.println("");
+        }
+        
+    }
+    
+}
