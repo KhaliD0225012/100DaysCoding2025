3864e057d44dce2f4ffd2f6fadc75989cbb719d7
KhaliD0225012 <aleph3371@gmail.com>
2025-11-26 20:33:48 +0800

    Create Day80Coding.java

---
+++ Day80Coding.java
@@ -0,0 +1,23 @@
+package day80coding;
+
+public class Day80Coding {
+
+    public static void main(String[] args) {
+        
+        String a[] = new String[5];
+        a[0] = "Satu";
+        a[1] = "Dua";
+        a[2] = "Tiga";
+        a[3] = "Emapat";
+        a[4] = "Lima";
+        int b[] = {1, 2, 3, 4, 5};
+        
+        System.out.printf("%d. %s\n", b[0], a[0]);
+        System.out.printf("%d. %s\n", b[1], a[1]);
+        System.out.printf("%d. %s\n", b[2], a[2]);
+        System.out.printf("%d. %s\n", b[3], a[3]);
+        System.out.printf("%d. %s\n", b[4], a[4]);
+        
+    }
+    
+}
