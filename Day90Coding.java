d6a9feb1f1603c00732fd2d1139f9befa531d08f
KhaliD0225012 <aleph3371@gmail.com>
2025-12-06 19:54:51 +0800

    Create Day90Coding.java

---
+++ Day90Coding.java
@@ -0,0 +1,15 @@
+package day90coding;
+
+public class Day90Coding {
+
+    public static void main(String[] args) {
+        
+        System.out.println(a()*a());
+        
+    }
+    
+    static int a(){
+        return 3;
+    }
+    
+}
