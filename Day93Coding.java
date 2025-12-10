6660ea0b46cfcedde3f3dc10fefbe02b9f72ce09
KhaliD0225012 <aleph3371@gmail.com>
2025-12-10 10:21:56 +0800

    Create Day93Coding.java

---
+++ Day93Coding.java
@@ -0,0 +1,17 @@
+package day93coding;
+
+public class Day93Coding {
+
+    public static void main(String[] args) {
+        
+        double a = 1.4;
+        double b = 2.5;
+        double c = 3.6;
+        
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.floor(b));
+        System.out.println(Math.round(c));
+        
+    }
+    
+}
